5fe6a76294d1fb52149012ee25d260c79826fba1
Prosper Nnadi <prospernnadi3@gmail.com>
2022-03-07 01:48:06 +0100

    Final Commit, added event to close modal on ESC keypress

--- script.js
+++ script.js
@@ -21,3 +21,7 @@ for(let i = 0; i < showModal.length; i++) {
 
 close.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e){
+    if(e.key === 'Escape' && (!modal.classList.contains('hidden'))) closeModal();
+})
