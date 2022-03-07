70e64fa5b97c48cdae55391df0643e8e1069df15
Prosper Nnadi <prospernnadi3@gmail.com>
2022-03-07 01:35:02 +0100

    Final Commit

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const showModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const close = document.querySelector('.close-modal');
+
+const openModal = function() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModal = function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+for(let i = 0; i < showModal.length; i++) {
+    showModal[i].addEventListener('click', openModal);
+}
+
+close.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
